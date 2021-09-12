b0dfc4db6aa34df9aba66a7301b0156eb450a6fe
Jacob Felknor <jacobfelknor073@gmail.com>
2021-09-12 13:46:53 -0600

    more aws

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,4 +4,15 @@ from .local import *  # noqa
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["gastrackerapi.com"]
+ALLOWED_HOSTS = ["gastrackerapi.com", "127.0.0.1"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "gastrackerdb",
+        "USER": get_parameters_aws("gastrackerdb_user"),
+        "PASSWORD": get_parameters_aws("gastrackerdb_password"),
+        "HOST": get_parameters_aws("gastrackerdb_endpoint"),
+        "PORT": "3306",
+    }
+}
